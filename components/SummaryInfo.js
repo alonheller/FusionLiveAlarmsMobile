2628c58f4ded2d9b69c06e1113f36b7289c27ae3
alon.heller@gmail.com <alon.heller@gmail.com>
2021-03-02 14:36:46 +0200

    Badges Flex row

--- components/SummaryInfo.js
+++ components/SummaryInfo.js
@@ -4,27 +4,21 @@ import { Badge } from 'react-native-elements';
 
 const SummaryInfo: () => React$Node = () => {
 	return (
-		<View style={styles.container}>
-			<View style={[{ flex: 1 }, styles.elementsContainer]}>
-				<Badge value='3' status='success' />
-				<Badge value='99+' status='error' />
-				<Badge value='5' status='primary' />
-				<Badge value='22' status='warning' />
-			</View>
+		<View style={[styles.elementsContainer]}>
+			<Badge value='3' status='success' />
+			<Badge value='99+' status='error' />
+			<Badge value='5' status='primary' />
+			<Badge value='22' status='warning' />
 		</View>
 	);
 };
 
 const styles = {
-	container: {
-		marginTop: 48,
-		flex: 1
-	},
 	elementsContainer: {
-		backgroundColor: '#ecf5fd',
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
 		marginLeft: 24,
-		marginRight: 24,
-		marginBottom: 24
+		marginRight: 24
 	}
 };
 
